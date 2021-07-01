6e5e63635f08a268f9cc04baa151dbc4fc467a86
stevensurjadi1101 <stevensurjadi1101@gmail.com>
2021-07-01 20:34:22 +0700

    custom-build-1

---
+++ packages/custom-build-1/src/ckeditor.js
@@ -0,0 +1,19 @@
+/**
+ * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import BalloonEditor from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+
+class Editor extends BalloonEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Essentials,
+	Mention,
+	Paragraph
+];
+
+export default Editor;
